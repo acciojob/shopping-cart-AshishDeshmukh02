53928d4c04874fb86f46339dc5799cd15ab16d9b
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-05 15:04:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,25 @@
-//your code here
-var itemName = document.getElementById("item-name-input");
-var itemQuantity = document.getElementById("item-qty-input");
-var itemPrice = document.getElementById("item-price-input");
- 
-var tbody = document.querySelector("tbody");
- 
-var grandTotal = document.getElementById("total");
-var grand_total = 0;
- 
-var addItem = document.querySelector("#add");
- 
-addItem.addEventListener("click", (event) => {
-  event.preventDefault();
- 
-  let newRow = document.createElement("tr");
- 
-  let name = document.createElement("td");
-  name.innerText = itemName.value;
-  name.classList.add("item");
-  newRow.append(name);
- 
-  let price = document.createElement("td");
-  price.innerText = itemPrice.value;
-  newRow.append(price);
-  price.classList.add("price");
- 
-  let quantity = document.createElement("td");
-  quantity.innerText = itemQuantity.value;
-  newRow.append(quantity);
- 
-  if (itemName.value && itemQuantity.value && itemPrice.value) {
-    let total = document.createElement("td");
-    total.innerText = itemPrice.value * itemQuantity.value;
-    newRow.append(total);
- 
-    tbody.append(newRow);
- 
-    grand_total += Number(total.innerText);
-    grandTotal.innerHTML = "Grand Total: ₹ " + grand_total;
-  }
- 
-  itemName.value = "";
-  itemQuantity.value = "";
-  itemPrice.value = "";
-});
+const addButton = document.getElementById('add');
+      const itemInput = document.getElementById('item-name-input');
+      const priceInput = document.getElementById('item-price-input');
+      const list = document.getElementById('list');
+      const totalAmount = document.getElementById('total-amount');
+
+      addButton.addEventListener('click', () => {
+        if (!itemInput.value || !priceInput.value) {
+          alert('Both item name and price are required!');
+          return;
+        }
+
+        const newRow = document.createElement('tr');
+        const itemCell = document.createElement('td');
+        itemCell.innerText = itemInput.value;
+        newRow.appendChild(itemCell);
+        const priceCell = document.createElement('td');
+        priceCell.innerText = priceInput.value;
+        newRow.appendChild(priceCell);
+        list.insertBefore(newRow, list.children[list.children.length - 1]);
+        totalAmount.innerText = parseFloat(totalAmount.innerText) + parseFloat(priceInput.value);
+
+        itemInput.value = '';
+        priceInput.value = '';
+      });
